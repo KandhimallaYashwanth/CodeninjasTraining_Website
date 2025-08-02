44e912fbb657b09305d031f668da5d66bf9ab34a
Hareeswar <hareeswar0816@gmail.com>
2025-08-02 18:08:45 +0530

    Completed all courses pages

--- backend/routes/coursesRoutes.js
+++ backend/routes/coursesRoutes.js
@@ -1,7 +1,16 @@
 import express from 'express';
-import { getCoursesPage,getDataSciencePage } from '../controllers/coursesController.js';
+import { getCoursesPage, getDataSciencePage, getPythonPage, getAIPage, getWebDevPage, getJavaPage, getStockMarketPage, getFinancePage, getHumanResourcesPage } from '../controllers/coursesController.js';
 
 const router = express.Router();
+
 router.get('/courses', getCoursesPage);
 router.get('/courses/tech-courses/data-science-using-python',getDataSciencePage);
+router.get('/courses/tech-courses/python-programming', getPythonPage);
+router.get('/courses/tech-courses/artificial-intelligence', getAIPage);
+router.get('/courses/tech-courses/web-development', getWebDevPage);
+router.get('/courses/tech-courses/java-programming', getJavaPage);
+router.get('/courses/management-courses/stock-market', getStockMarketPage);
+router.get('/courses/management-courses/finance', getFinancePage);
+router.get('/courses/management-courses/human-resources', getHumanResourcesPage);
+
 export default router;
